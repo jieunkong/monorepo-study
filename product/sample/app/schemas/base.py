74e273c140bac3b65365ffc651b6fd7dabb090b5
vuno <jieun.kong@vuno.co>
2022-09-29 17:43:31 +0900

    Add schema

---
+++ product/sample/app/schemas/base.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class BaseRequest(BaseModel):
+    """
+    request data format
+    """
+    name: str
+
+
+class BaseReponse(BaseModel):
+    """
+    response data format
+    """
+    name: str
+    
